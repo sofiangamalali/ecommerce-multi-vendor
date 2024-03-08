c1960a05f7a81a045232a0051d25ed68172fb4b4
khaledali0907 <khaledali0907@gmail.com>
2024-03-08 21:26:21 +0200

    code

--- app/Http/Controllers/api/VendorController.php
+++ app/Http/Controllers/api/VendorController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\{Vendor};
 use App\Models\Plan;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Spatie\LaravelIgnition\Solutions\SolutionProviders\ViewNotFoundSolutionProvider;
 use Validator;
 use App\Mail\WelcomeMailForVendor;
 
@@ -135,16 +133,6 @@ class VendorController extends Controller
     public function getAllData()
     {
         $vendor = auth('vendor')->user();
-        $products = $vendor->products->all();
-        $ratings = [];
-
-        foreach ($products as $product) {
-            $DBproduct = Product::find($product->id);
-            $DBratings = $DBproduct->rating->all();
-            if (count($DBratings) > 0)
-                $ratings[$product->id] = $DBratings;
-        }
-
         return response()->json(
             [
                 "message" => "success",
@@ -152,9 +140,7 @@ class VendorController extends Controller
                     'numberOfProducts' => $vendor->products()->count(),
                     'plan' => Plan::find($vendor->plan_id)->name,
                     'business_name' => $vendor->business_name,
-                    'accountStatus' => $vendor->is_active ? 'Verified' : 'Suspended',
-                    'ratings' => $ratings,
-                    'ratingsCount' => count($ratings),
+                    'accountStatus'=>$vendor->is_active?'Verified':'Suspended',
                 ]
             ],
             200
@@ -166,7 +152,7 @@ class VendorController extends Controller
         $vendorData= Vendor::find($vendor->id);
 
         return response()->json(['data' => $vendorData],200);
-        
+
     }
 
 }
